4b00095a58f15b5b2eb20e0dfe168bb548d963a4
chairich <33069090+chairich@users.noreply.github.com>
2025-07-01 16:51:33 +0700

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,18 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyBUNNtOVjCq40fneEp-DbDCVtIVGBVI6Pg",
+  authDomain: "pet-project-1d196.firebaseapp.com",
+  projectId: "pet-project-1d196",
+  storageBucket: "pet-project-1d196.firebasestorage.app",
+  messagingSenderId: "261670745146",
+  appId: "1:261670745146:web:eaf0ff381b1184bdee3103"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const auth = getAuth(app);
